9354570889dccc689677acf8c33575287da05292
alexandre <alexandre.candido.xavier.ufes@gmail.com>
2023-10-31 15:40:56 -0300

    cod_py8

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = '2023, Alexandre C Xavier'
 author = 'Alexandre C Xavier'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.2'
 
 
 # -- General configuration ---------------------------------------------------
